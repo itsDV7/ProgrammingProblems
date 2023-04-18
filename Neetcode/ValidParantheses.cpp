41a3e0ab8150bd079222b46ef1a2794202cc241e
Divyansh Chaudhary <divyansh7c@gmail.com>
2023-04-18 13:39:04 +0530

    Create ValidParantheses.cpp

---
+++ Neetcode/ValidParantheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c: s){
+            if (stk.empty()){
+                stk.push(c);
+            }
+            else{
+                if (stk.top() == '(' && c == ')'){
+                    stk.pop();
+                }
+                else if (stk.top() == '{' && c == '}'){
+                    stk.pop();
+                }
+                else if (stk.top() == '[' && c == ']'){
+                    stk.pop();
+                }
+                else{
+                    stk.push(c);
+                }
+            }
+        }
+        if (stk.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
