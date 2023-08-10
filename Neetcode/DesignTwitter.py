0e04c8cdb1f4049dd0d52868ec31ed6e3a390000
Divyansh Chaudhary <divyansh7c@gmail.com>
2023-08-11 04:45:54 +0530

    Update DesignTwitter.py

--- Neetcode/DesignTwitter.py
+++ Neetcode/DesignTwitter.py
@@ -1,44 +1,45 @@
 class Twitter:
 
     def __init__(self):
-        self.followed_by = dict()
-        self.user_feed = dict()
+        self.userFollows = dict()
+        self.userPosts = dict()
+        self.postTime = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.follow(userId, userId)
-        for user in self.followed_by[userId]:
-            self.user_feed[user].append((userId, tweetId))
+        self.userPosts[userId].append((self.postTime, tweetId))
+        self.postTime -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        feed = 0
-        i = len(self.user_feed[userId]) - 1
-        ans = list()
-        while i > -1 and feed < 10:
-            if self.user_feed[userId][0] in self.followed_by[userId]:
-                ans.append(self.user_feed[userId][1])
-                feed += 1
-            i -= 1
-        return ans
+        self.follow(userId, userId)
+        posts = list()
+        for user in self.userFollows[userId]:
+            for post in self.userPosts[user]:
+                posts.append(post)
+        heapq.heapify(posts)
+        feed = list()
+        while posts and len(feed) < 10:
+            feed.append(heapq.heappop(posts)[1])
+        return feed
 
     def follow(self, followerId: int, followeeId: int) -> None:
         
-        if followerId not in self.followed_by:
-            self.followed_by[followerId] = set({followerId})
-        if followeeId not in self.followed_by:
-            self.followed_by[followeeId] = set({followeeId})
+        if followerId not in self.userFollows:
+            self.userFollows[followerId] = set({followerId})
+            self.userPosts[followerId] = list()
+
+        if followeeId not in self.userFollows:
+            self.userFollows[followeeId] = set({followeeId})
+            self.userPosts[followeeId] = list()
         
-        self.followed_by[followeeId].add(followerId)
-        
-        if followerId not in self.user_feed:
-            self.user_feed[followerId] = list()
-        if followeeId not in self.user_feed:
-            self.user_feed[followeeId] = list()
+        self.userFollows[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
+        
         if followerId != followeeId:
-            if followeeId in self.followed_by:
-                if followerId in self.followed_by[followeeId]:
-                    self.followed_by[followeeId].remove(followerId)
+            if followerId in self.userFollows:
+                if followeeId in self.userFollows[followerId]:
+                    self.userFollows[followerId].remove(followeeId)
 
 
 # Your Twitter object will be instantiated and called as such:
